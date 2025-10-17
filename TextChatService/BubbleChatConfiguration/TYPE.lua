4005e89d338f296af884d020de51215a2365d141
articulatedreality-lang <articulatedreality@gmail.com>
2025-10-17 22:51:13 +0530

    Create TYPE.lua

---
+++ TextChatService/BubbleChatConfiguration/TYPE.lua
@@ -0,0 +1,4 @@
+--[[ This object has 4 children, being, a UICorner, a UIGradient, a UIPadding and an ImageLabel, but because I will use custom chat for the game, This Bubble Chat is of no use to me, and thus, folders corresponding
+to those objects have been omitted. ]]--
+
+return "BubbleChatConfiguration"
