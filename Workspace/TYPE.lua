49d7f932690fbe3eb76c963cd5d6c3fcb6d8d890
articulatedreality-lang <articulatedreality@gmail.com>
2025-10-17 22:54:52 +0530

    Create TYPE.lua

--- Workspace/TYPE.lua
+++ Workspace/TYPE.lua
@@ -1,1 +1,4 @@
+--[[ The workspace contains a `{Camera}`, a Baseplate (which contains a `{Decal}`), a `{Terrain}` instance and a `{SpawnLocation}` (which contains another
+`{Decal}`) but those are default objects, and camera is just studio's camera (not in-game, that's CurrentCamera), and thus their folders are omitted. ]]--
+
 return "Workspace"
